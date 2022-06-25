3f4f8475ba88b7d84f30ee4a11319511868ee233
Ejay02 <e.jae02@gmail.com>
2022-06-25 21:13:08 +0100

    Ip tracker setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
+    <div
+      class="flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32 bg-red-500"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-whit text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+          <input
+            class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none"
+            type="text"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
